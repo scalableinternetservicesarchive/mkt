c17d7c863f5aa8a94352af8a0c9f36ad8479282f
Param Shah <paramshah09@gmail.com>
2020-11-24 13:18:12 +0530

    k6 script init

---
+++ script.js
@@ -0,0 +1,70 @@
+import http from 'k6/http'
+import { Counter, Rate } from 'k6/metrics'
+
+export const options = {
+  scenarios: {
+    example_scenario: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '60s', target: 5000 },
+        { duration: '60s', target: 0 },
+      ],
+      gracefulRampDown: '0s',
+    },
+  },
+}
+// export const options = {
+//   scenarios: {
+//     example_scenario: {
+//       executor: 'constant-vus',
+//       vus: 1000,
+//       duration: '30s',
+//     },
+//   },
+// }
+
+export default function () {
+  // recordRates(
+  // const resp = http.post(
+  //   'http://localhost:3000/graphql',
+  //   '{"operationName":"AnswerSurveyQuestion","variables":{"input":{"answer":"🤗","questionId":1}},"query":"mutation AnswerSurveyQuestion($input: SurveyInput!) {\\n  answerSurvey(input: $input)\\n}\\n"}',
+  //   {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   }
+  // )
+
+  recordRates(
+    http.get('http://localhost:3000/app/')
+  )
+  sleep(Math.random() * 3)
+}
+
+const count200 = new Counter('status_code_2xx')
+const count300 = new Counter('status_code_3xx')
+const count400 = new Counter('status_code_4xx')
+const count500 = new Counter('status_code_5xx')
+
+const rate200 = new Rate('rate_status_code_2xx')
+const rate300 = new Rate('rate_status_code_3xx')
+const rate400 = new Rate('rate_status_code_4xx')
+const rate500 = new Rate('rate_status_code_5xx')
+
+function recordRates(res) {
+  if (res.status >= 200 && res.status < 300) {
+    count200.add(1)
+    rate200.add(1)
+  } else if (res.status >= 300 && res.status < 400) {
+    console.log(res.body)
+    count300.add(1)
+    rate300.add(1)
+  } else if (res.status >= 400 && res.status < 500) {
+    count400.add(1)
+    rate400.add(1)
+  } else if (res.status >= 500 && res.status < 600) {
+    count500.add(1)
+    rate500.add(1)
+  }
+}
