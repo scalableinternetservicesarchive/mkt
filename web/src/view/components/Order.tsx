929310201d2f1408b2fa46c620691570ac681e3f
Srivishnu Ramamurthi <srivishnur@ucla.edu>
2020-12-07 13:40:01 -0800

    Aws fixes

--- web/src/view/components/Order.tsx
+++ web/src/view/components/Order.tsx
@@ -25,7 +25,7 @@ export function Order(props: OrderProps & Props) {
   return (
     <Card
       onClick={() => {
-        navigate(`post/${id}`).catch(err => {
+        navigate(`app/post/${id}`).catch(err => {
           console.log('error', err)
         })
       }}
